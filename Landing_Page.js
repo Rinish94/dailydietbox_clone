db51db8f65f3a424e815d511770b9e7549b1a126
kamlendr <kamlendra92@gmail.com>
2021-03-12 16:40:22 +0530

    v2.0

--- Landing_Page.js
+++ Landing_Page.js
@@ -1,18 +1,3 @@
-// For sticky Navbar
-window.onscroll = function () { myFunction() };
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
-
-function myFunction() {
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
-    } else {
-        navbar.classList.remove("sticky");
-    }
-}
-// sticky COde ends here
-
-
 
 //meal photos animation starts here
 let mealImg = document.querySelectorAll('#mealContain img')
@@ -80,6 +65,56 @@ del.addEventListener('click', () => {
     }
 })
 //cart Item counter starts here
+
+
+//CHAT BOX starts here
+function talk() {
+    let know = {
+      Hi:
+        "Hi Scientific Person😊, Welcome back! Please select an option from the below menu.",
+      hi:
+        "Hi Scientific Person😊, Welcome back! Please select an option from the below menu.",
+      HELLO:
+        "Hi Scientific Person😊, Welcome back! Please select an option from the below menu.",
+      hello:
+        "Hi Scientific Person😊, Welcome back! Please select an option from the below menu.",
+      "How are you": "Good 😊",
+      "how are you": "Good 😊",
+      shakes: "Which flavour you want to buy?",
+      vanilla:
+        "We have a Vanilla Coffee Shakes with natural extracts, Shop Now !!! ",
+      berry:
+        "We have a Oatmeal Berry Shakes with natural extracts, Shop Now !!! ",
+      orange:
+        "We have a Valencia orange Shakes with natural extracts, Shop Now !!! ",
+      Orange:
+        "We have a Valencia orange Shakes with natural extracts, Shop Now !!! ",
+      apple: "We have a Rich Apple Shakes with natural extracts, Shop Now !!! ",
+      Apple: "We have a Rich Apple Shakes with natural extracts, Shop Now !!! ",
+      ok: "Thank You So Much 😊",
+      Bye: "Okay! Will meet soon. TC..",
+    };
+    let user = document.getElementById("userBox").value;
+    document.getElementById("chatLog").innerHTML = user + "<br>";
+    if (user in know) {
+      document.getElementById("chatLog").innerHTML = know[user] + "<br>";
+    } else {
+      document.getElementById("chatLog").innerHTML =
+        "Oops! I think you entered a wrong option. Please select an option from below <br>";
+    }
+  }
+  
+  let addBot = document.querySelector(".bot");
+  let modalBox = document.querySelector(".modal-box");
+  let modalCloseIcon = document.querySelector(".modal-close_icon");
+  
+  addBot.addEventListener("click", function () {
+    modalBox.classList.add("bg-on");
+  });
+  modalCloseIcon.addEventListener("click", function () {
+    modalBox.classList.remove("bg-on");
+  });
+//CHAT BOX ends here
 
 
 
